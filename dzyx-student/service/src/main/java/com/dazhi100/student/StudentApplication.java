80b73b0c39356d6c92743289674deb55d9d2f4b6
huangjianlou <huangjianlou@dazhi100.com>
2022-04-12 10:05:02 +0800

    student简单版脚手架

---
+++ dzyx-student/service/src/main/java/com/dazhi100/student/StudentApplication.java
@@ -0,0 +1,16 @@
+package com.dazhi100.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: huangjl
+ * @date: 2022/4/11
+ */
+@SpringBootApplication
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApplication.class, args);
+    }
+}
