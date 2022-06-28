c371d3848b631420bd3b54e69dfa6ddaea7871e9
wanglaibing <WLBSky@outlook.com>
2022-06-28 18:43:21 +0800

    去除错误注解

--- web/src/main/java/com/dazhi100/student/web/biz/entity/request/StudentRequest.java
+++ web/src/main/java/com/dazhi100/student/web/biz/entity/request/StudentRequest.java
@@ -1,14 +1,11 @@
 package com.dazhi100.student.web.biz.entity.request;
 
-import org.springframework.boot.context.properties.ConstructorBinding;
-
 import java.time.LocalDateTime;
 
 /**
  * @author: huangjl
  * @date: 2022/4/25
  */
-@ConstructorBinding
 public record StudentRequest(Long studentId, String studentName, LocalDateTime time, Long gradeId, String gradeName) {
 
 }
--- web/src/main/java/com/dazhi100/student/web/biz/entity/response/StudentResponse.java
+++ web/src/main/java/com/dazhi100/student/web/biz/entity/response/StudentResponse.java
@@ -1,14 +1,11 @@
 package com.dazhi100.student.web.biz.entity.response;
 
-import org.springframework.boot.context.properties.ConstructorBinding;
-
 import java.time.LocalDateTime;
 
 /**
  * @author: huangjl
  * @date: 2022/4/25
  */
-@ConstructorBinding
 public record StudentResponse(LocalDateTime time) {
 
 }
