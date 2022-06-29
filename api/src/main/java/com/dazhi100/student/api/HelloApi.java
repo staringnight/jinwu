000f7be9597cbbcea6efc08c8f75bb1565215471
wanglaibing <WLBSky@outlook.com>
2022-06-29 16:06:47 +0800

    类修改成record

--- api/src/main/java/com/dazhi100/student/api/HelloApi.java
+++ api/src/main/java/com/dazhi100/student/api/HelloApi.java
@@ -1,10 +1,12 @@
 package com.dazhi100.student.api;
 
+import com.dazhi100.student.api.dto.StudentDto;
+
 /**
  * @author: huangjl
  * @date: 2022/4/14
  */
 public interface HelloApi {
 
-    void sayHello();
+    void sayHello(StudentDto studentDto);
 }
